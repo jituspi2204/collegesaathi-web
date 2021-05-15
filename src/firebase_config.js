71a8c4e1bdc0b76cdbd7421b534f006276daf4e3
Ankit Vashisht <ankitvashisht12@gmail.com>
2021-05-15 15:41:34 +0530

    add firebase

---
+++ src/firebase_config.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+
+    const firebaseConfig = {
+        apiKey: "AIzaSyCWQnlvDngcNEIxCXvN-GWwkEmWn2Z7BIU",
+        authDomain: "collegesaathi-india.firebaseapp.com",
+        projectId: "collegesaathi-india",
+        storageBucket: "collegesaathi-india.appspot.com",
+        messagingSenderId: "91784748644",
+        appId: "1:91784748644:web:d80addf59acf66ee6dc971",
+        measurementId: "G-TT6JN5EVTE"
+      };
+
+      firebase.initializeApp(firebaseConfig)
+      export default firebase;
